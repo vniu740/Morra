eeb58758a86d83aa6e728341317f8bdc2ed4b9c2
VENXIA <vniu740>
2023-05-05 00:13:42 +1200

    Inputs, fingers and sum, are check and message is printed out if inputs are invalid until inputs are valid

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -4,14 +4,51 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
 
-  public Morra() {}
+  public Morra() {
+  }
+
+  private int roundCounter;
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
 
+    // Set the round to 1
+    roundCounter = 1;
+
+    // Print the message to welcome the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+
   }
 
-  public void play() {}
+  public void play() {
 
-  public void showStats() {}
+    // Print round being played
+    MessageCli.START_ROUND.printMessage(String.valueOf(roundCounter));
+    roundCounter++;
+
+    // Print give fingers and sum inputs
+    MessageCli.ASK_INPUT.printMessage();
+    String fingersSumInputString = Utils.scanner.nextLine();
+
+    boolean areInputsValid = false;
+
+    while (areInputsValid == false) {
+      // Split the input string into an array of string of the numbers (seperating by
+      // white space)
+      String[] arrayFingersSumInput = fingersSumInputString.split("\\s+");
+
+      if ((arrayFingersSumInput.length == 2) && (arrayFingersSumInput[0].matches("\\d+"))
+          && (arrayFingersSumInput[1].matches("\\d+"))
+          && ((Integer.parseInt(arrayFingersSumInput[0]) >= 1) && (Integer.parseInt(arrayFingersSumInput[0]) <= 5))
+          && ((Integer.parseInt(arrayFingersSumInput[1]) >= 1) && (Integer.parseInt(arrayFingersSumInput[1]) <= 10))) {
+        areInputsValid = true;
+      } else {
+        MessageCli.INVALID_INPUT.printMessage();
+        fingersSumInputString = Utils.scanner.nextLine();
+      }
+
+    }
+  }
+
+  public void showStats() {
+  }
 }
