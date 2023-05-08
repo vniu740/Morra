326e5b110e28d835b3a34102e8bd0a708c5498e3
VENXIA <vniu740>
2023-05-09 00:11:31 +1200

    Same as previous commit, forgot to add this file

--- src/main/java/nz/ac/auckland/se281/EasyDifficultyLevel.java
+++ src/main/java/nz/ac/auckland/se281/EasyDifficultyLevel.java
@@ -4,7 +4,7 @@ public class EasyDifficultyLevel implements DifficultyLevel {
 
     @Override
     public Integer[] playUsingLevel() {
-        CPU easyStrategy = new CPU(new RandomStrategy());
+        StrategyCreator easyStrategy = new StrategyCreator(new RandomStrategy());
 
         return easyStrategy.jarvisPlays();
     }
