d12c2e1c37fed50795e780befd18c76d862ead3d
VENXIA <vniu740>
2023-05-09 16:55:53 +1200

    Implemented TopStrategy.java

--- src/main/java/nz/ac/auckland/se281/Strategy.java
+++ src/main/java/nz/ac/auckland/se281/Strategy.java
@@ -1,6 +1,6 @@
 package nz.ac.auckland.se281;
 
 public interface Strategy {
-    public Integer[] decideSumAndFingers(int average);
+    public Integer[] decideSumAndFingers(int averageOrTop);
 
 }
--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -3,8 +3,13 @@ package nz.ac.auckland.se281;
 public class TopStrategy implements Strategy {
 
     @Override
-    public Integer[] decideSumAndFingers(int humanFingers) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'decideSumAndFingers'");
+    public Integer[] decideSumAndFingers(int top) {
+        Integer jarvisFingers;
+        Integer jarvisSum;
+
+            jarvisFingers = Utils.getRandomNumber(1, 5);
+            jarvisSum = jarvisFingers + top;
+
+            return new Integer[] {jarvisFingers, jarvisSum };
     }
 }
