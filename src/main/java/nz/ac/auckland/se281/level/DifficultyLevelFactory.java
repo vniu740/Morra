9507d04b1db0cd5767dd19ce5a09c2415ee54afb
VENXIA <vniu740>
2023-05-22 18:17:33 +1200

    Fixed indentation

--- src/main/java/nz/ac/auckland/se281/level/DifficultyLevelFactory.java
+++ src/main/java/nz/ac/auckland/se281/level/DifficultyLevelFactory.java
@@ -4,29 +4,29 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class DifficultyLevelFactory {
 
-    // Method for determining which DifficultyLevel Instance should be created
-    public static DifficultyLevel createLevel(Difficulty level) {
+  // Method for determining which DifficultyLevel Instance should be created
+  public static DifficultyLevel createLevel(Difficulty level) {
 
-        switch (level) {
-            // If input is easy, create new EasyDifficultyLevel instance
-            case EASY:
-                return new EasyDifficultyLevel();
+    switch (level) {
+      // If input is easy, create new EasyDifficultyLevel instance
+      case EASY:
+        return new EasyDifficultyLevel();
 
-            // If input is hard, create new HardDifficultyLevel instance
-            case HARD:
-                return new HardDifficultyLevel();
+      // If input is hard, create new HardDifficultyLevel instance
+      case HARD:
+        return new HardDifficultyLevel();
 
-            // If input is Master, create new MasterDifficultyLevel instance
-            case MASTER:
-                return new MasterDifficultyLevel();
+      // If input is Master, create new MasterDifficultyLevel instance
+      case MASTER:
+        return new MasterDifficultyLevel();
 
-            // If input is medium, create new MediumDifficultyLevel instance
-            case MEDIUM:
-                return new MediumDifficultyLevel();
-
-        }
-        return null;
+      // If input is medium, create new MediumDifficultyLevel instance
+      case MEDIUM:
+        return new MediumDifficultyLevel();
 
     }
+    return null;
+
+  }
 
 }
